92906c0b0a2c6dd4c94dc7bd67159c02a6bf5c6d
emibroder <emilianostef@gmail.com>
2025-06-17 23:19:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,82 +4,81 @@ import groq  #API
 
 MODELOS = ['llama3-8b-8192', 'llama3-70b-8192','mixtral-8x7b-32768']
 
-
-
 # CONFIGURAR PAGINA
 def configurar_pagina():
-    st.set_page_config(page_title="Mi Primer Chatbot con Python ")
+    st.set_page_config(page_title="Mi Pirmer ChatBot con Python")
     st.title("Bienvenidos a mi Chatbot")
 
-
-    # CREAR UN CLIENTE DE GROQ
-    def crear_cliente_groq():
-        groq_api_keys = st.secrets["Groq_APY_KEY"]
-        return groq.Groq(api_key=groq_api_keys)
+# CREAR UN CLIENTE GROQ => NOSOTROS
+def crear_cliente_groq():
+    groq_api_key = st.secrets["GROQ_API_KEY"]
+    return groq.Groq(api_key=groq_api_key)
     
-    #MOSTRAR LA BARRA LATERAL
-    def mostrar_sidebar():
-        st.sidebar.title("Elegi tu modelo de IA")
-        modelo = st.sidebar.selectbox ("",MODELOS,index=0)
-        st.write(f'**Elegiste el modelo** {modelo}')
-        return modelo 
-    
-    #INICIALIZAR EL ESTADO DEL CHAT
-    def inicializar_estado_chat():
-        if "mensajes" not in st.session_state:
-            st.session_state.mensajes =[]
+# MOSTRAR LA BARRA LATERAL 
+def mostrar_sidebar():
+    st.sidebar.title("Elegí tu modelo de IA favorito")
+    modelo = st.sidebar.selectbox('elegí tu modelo',MODELOS,index=0)
+    st.write(f'**Elegiste el modelo** {modelo}')
+    return modelo
 
-    #MOSTRAR MENSAJES REVIOS
-    def obtener_mensajes_previos():
-        for mensaje in st.session_state.mensajes:
-            with st.chat_message(mensaje["role"]):
-                st.markdown(mensaje["content"])
-    #OBTENER MENSAJE USUARIO
-    def obtener_mensaje_usuario():
-        return st.chat_input("Envia tu mensaje")
-    
-    #GUARDAR LOS MENSAJES
-    def agregar_mensajes_previos(role,content):
-        st.session_state.mensajes.append({"role": role , "content": content})
+#INICIALIZAR EL ESTADO DEL CHAT
+#streamlit => variable especial llamada session_state. {mensajes => []}
+def inicializar_estado_chat():
+    if "mensajes"  not in st.session_state:
+        st.session_state.mensajes = [] #lista
+#MOSTRAR MENSAJES REVIOS
 
-    #MOSTRAR LOS MENSAJES EN PANTALLA
-    def mostrar_mensaje(role, content):
-        with st.chat_message(role):
-            st.markdown(content)
+def obtener_mensajes_previos():
+    for mensaje in st.session_state.mensajes: # recorrer los mensajes de st.session_state.mensaje
+        with st.chat_message(mensaje["role"]): #quien lo envia ??
+            st.markdown(mensaje["content"]) #que envia?
 
+#OBTENER MENSAJE USUARIO
+def obtener_mensaje_usuario():
+    return st.chat_input("Envia tu mensaje")
 
-    #CREACION DEL MODELO DE GROQ
+#GUARDAR LOS MENSAJES
+def agregar_mensajes_previos(role, content):
+    st.session_state.mensajes.append({"role": role , "content": content})
 
-    def obtener_respuesta_modelo(cliente, modelo, mensaje):
-        respuesta = cliente.chat.completions.create(
-            model = modelo,
-            messages = mensaje,
-            stream = False 
-        )
-        return respuesta.choices[0].message.content
+#MOSTRAR LOS MENSAJES EN PANTALLA
+def mostrar_mensaje(role, content):
+    with st.chat_message(role):
+        st.markdown(content)
     
 
+#llamar DEL MODELO DE GROQ
+def obtener_respuesta_modelo(cliente, modelo, mensaje):
+    respuesta =  cliente.chat.completions.create(
+        model = modelo,
+        messages = mensaje,
+        stream= False
+    )
+    return respuesta.choices[0].message.content
+    
+    
+    
 
+def ejecutar_chat():
+    configurar_pagina()
+    cliente = crear_cliente_groq()
+    modelo = mostrar_sidebar()
+    
+    inicializar_estado_chat()
+    mensaje_usuario = obtener_mensaje_usuario()
+    obtener_mensajes_previos()
+    
+    if mensaje_usuario:
+        agregar_mensajes_previos("user",mensaje_usuario)
+        mostrar_mensaje("user",mensaje_usuario)
+    
+        respuesta_contenido = obtener_respuesta_modelo(cliente, modelo,st.session_state.mensajes )
 
-    def ejecutar_chat():
-        configurar_pagina()
-        cliente = crear_cliente_groq()
-        modelo = mostrar_sidebar()
-
-        inicializar_estado_chat()
-        mensaje_usuario = obtener_mensaje_usuario()
-        obtener_mensajes_previos()
-
-        if mensaje_usuario:
-            agregar_mensajes_previos("user",mensaje_usuario)
-            mostrar_mensaje("user",mensaje_usuario)
-
-            respuesta_contenido = obtener_respuesta_modelo(cliente, modelo,st.session_state.mensajes ) 
-        
-            agregar_mensajes_previos("assistant",respuesta_contenido)
-            mostrar_mensaje("assistant",respuesta_contenido)
-
-    # EJECUTAR LA APP( si __name__ es igual a __main__ se ejecuta la funcion, y __main__ es mi archivo principal)
-    if __name__ == '__main__':
-        ejecutar_chat()
+        agregar_mensajes_previos("assistant",respuesta_contenido)
+        mostrar_mensaje("assistant",respuesta_contenido)
+    
+    
+# EJECUTAR LA APP( si __name__ es igual a __main__ se ejecuta la funcion, y __main__ es mi archivo principal)
+if __name__ == '__main__':
+    ejecutar_chat()
         
